823eabb78b18ea3c19025e9e56480cdd61df7f9b
JaidenCook <jaiden.cook@student.curtin.edu.au>
2019-11-09 23:15:29 +0800

    Added the ability to have Gaussian models

--- vo2newmodel_2.py
+++ vo2newmodel_2.py
@@ -155,7 +155,7 @@ with open(options.output,"a") as f:
 
         if shape=="gaussian":
 
-            f.write(pformatter.format(Name=Name,RA=RA,Dec=Dec,a=a,b=b,pa=pa,flux=flux,coeff_flippd=coeff_flippd,\
+            f.write(gformatter.format(Name=Name,RA=RA,Dec=Dec,a=a,b=b,pa=pa,flux=flux,coeff_flippd=coeff_flippd,\
 freq=options.freq,shape=shape))
 
         elif shape=="point":
